2d036516837207d0eab4f0dc73f3ae4133566712
KhuloodBatis <k.batis@bimventures.com>
2023-01-13 11:28:30 +0300

    add tweet

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,14 +1,14 @@
 <div class="flex p-4 border-b border-b-gray-400 ">
     <div class="mr-2 flex-shrink-0">
         <img
-        src="https://i.pravatar.cc/50"
+        src="{{auth()->user()->avatar}}"
         alt=""
         class="rounded-full mr-2"
         >
     </div>
     <div>
-        <h5 class="font-blod mb-4">Koolfzr</h5>
-        <p class="text-sm">From ugly racism to dangerous QAnon conspiracies to indefensible Holocaust denialism, hatred has taken root and spread across Facebook.</p>
+        <h5 class="font-blod mb-4">{{$tweet->user->name}}</h5>
+        <p class="text-sm">{{$tweet->body}}</p>
     </div>
 
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
    <div class="md:flex md:justify-between">
-    <div class="md:w-1/6">
+    <div class="md:w-32">
         @include('_sidebar-links')
     </div>
     <div class="md:flex-1 md:mx-10" style="max-width: 700px">
 
             @include('_publish-tweet-panel')
         <div class="border border-gray-300 rounded-lg">
-        @include("_tweet")
-        @include("_tweet")
-        @include("_tweet")
-        @include("_tweet")
-        @include("_tweet")
+            @foreach ($tweets as $tweet )
+            @include("_tweet")
+
+            @endforeach
+
         </div>
     </div>
-    <div class="md:w-1/6 bg-blue-100 rounded-lg p-4">
+    <div class="md:w-1/6 bg-blue-100 rounded-lg p-4 h-full">
         @include('_friends-list')</div>
 
    </div>
